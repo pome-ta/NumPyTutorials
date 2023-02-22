55fc66c88edbc7465ac12e40244b1e81f6d99a5a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-22 10:41:20 +0900

    up

---
+++ sandbox/230222_1041.py
@@ -0,0 +1,16 @@
+import numpy as np
+from PIL import Image as ImageP
+
+UINT_MAX = 0xffffffff
+
+u = np.array([UINT_MAX + 1], dtype=np.uint32)
+
+sq_size = 64
+np_canvas = np.zeros((sq_size, sq_size, 3), dtype=np.float32)
+
+p_canvas = np.zeros((sq_size, sq_size, 3), dtype=np.uint8)
+
+# init_img = ImageP.new('RGB', (sq_size, sq_size))
+# base_array = np.asarray(init_img)
+imgp = ImageP.fromarray(p_canvas)
+
