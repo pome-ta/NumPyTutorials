537f11cf763552d4904c32bf3aaf3c6392af554c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-23 18:27:58 +0900

    up

---
+++ sandbox/230223_1753.py
@@ -0,0 +1,38 @@
+import numpy as np
+from PIL import Image as ImageP
+
+
+sq_size = 320
+RGB_SIZE = 256
+color_ch = 3
+
+length_size = sq_size * sq_size * color_ch
+
+canvasnp = np.zeros((sq_size, sq_size, 3), dtype=np.uint8)
+
+xx = np.array(
+  [[int(x / sq_size * RGB_SIZE) for x in range(sq_size)]
+   for _ in range(sq_size)],
+  dtype=np.uint8)
+yy = np.array(
+  [[int(y / sq_size * RGB_SIZE) for _ in range(sq_size)]
+   for y in range(sq_size)],
+  dtype=np.uint8)
+
+
+canvasnp[:, :, 0] = xx
+canvasnp[:, :, 1] = xx
+canvasnp[:, :, 2] = xx
+
+
+#zero = np.array(np.np.linspace)
+
+zero = np.array(
+  np.linspace(0, 1, num=length_size),
+  dtype=np.float32).reshape(sq_size, sq_size, color_ch)
+
+
+imgp = ImageP.fromarray(canvasnp)
+
+_ = 1
+
