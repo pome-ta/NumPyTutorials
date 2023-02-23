1bddc0a4c97fa7a1115e2e4806b22d1861c2de5d
pome-ta <pometarou93@gmail.com>
2023-02-23 16:56:28 +0900

    up

--- sandbox/230223_1355.py
+++ sandbox/230223_1355.py
@@ -9,9 +9,12 @@ color_ch = 3
 
 length_size = sq_size * sq_size * color_ch
 
-xx = np.array([[x / sq_size * RGB_SIZE for x in range(sq_size)] for _ in range(sq_size)])
+xx = np.array([[int(x / sq_size * RGB_SIZE) for x in range(sq_size)] for _ in range(sq_size)], dtype=np.uint8)
 yy = np.array([[y for _ in range(sq_size)] for y in range(sq_size)])
 x = np.linspace(0, RGB_SIZE, num=sq_size)
+
+
+
 
 #imgp = ImageP.fromarray(uary)
 
