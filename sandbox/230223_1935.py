b2c87865233934072ee08e53d40053dd29d8cf9f
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-23 22:07:33 +0900

    blank

--- sandbox/230223_1935.py
+++ sandbox/230223_1935.py
@@ -16,12 +16,14 @@ height = sq_size
 
 # x = np.full((sq_size, sq_size), np.array([x_pos, y_pos]))
 _pos_x, _pos_y = np.meshgrid(np.arange(0, sq_size), np.arange(0, sq_size).reshape(sq_size, 1))
-pos = np.empty((sq_size, sq_size, 2), dtype=np.uint32)
+pos = np.empty((sq_size, sq_size, 3), dtype=np.uint8)
 pos[:, :, 0] = _pos_x
 pos[:, :, 1] = _pos_y
+pos[:, :, 2] = 0
 
+imgp = ImageP.fromarray(pos)
 # print(pos[:, :, 1])
 
-print(pos[0][255])
+#print(pos[0][255])
 
 _ = 1
