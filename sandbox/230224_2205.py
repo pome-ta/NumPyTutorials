2003e8862ed2f11c5da70fde4f0f676506b9c6bf
pome-ta <pometarou93@gmail.com>
2023-02-25 00:46:19 +0900

    xy <-> yx

--- sandbox/230224_2205.py
+++ sandbox/230224_2205.py
@@ -7,7 +7,7 @@ from pprint import pprint
 UINT_MAX = 0xffff_ffff
 k = 0x4567_89ab
 
-sq_size: int = 4
+sq_size: int = 3
 
 width_size = sq_size
 height_size = sq_size
@@ -16,27 +16,28 @@ RGB_SIZE = 255
 color_ch = 3
 
 
-def _vec(x):
-  _vec_array = np.empty(x).astype(np.float32)
-
-
-def vec(x):
-  pass
+#
+# def _vec(x):
+#     _vec_array = np.empty(x).astype(np.float32)
+#
+#
+# def vec(x):
+#     pass
 
 
 def FragCoord(width, height) -> np.array:
-  _row = np.arange(0, width)
-  _col = np.arange(0, height).reshape(height, 1)
-  _x, _y = np.meshgrid(_row, _col)
-  _pos = np.empty((width, height, 2)).astype(np.float32)
-  _pos[:, :, 0] = _x
-  _pos[:, :, 1] = _y
-  return _pos
+    _row = np.arange(0, width)
+    _col = np.arange(0, height).reshape(height, 1)
+    _x, _y = np.meshgrid(_row, _col)
+    _pos = np.empty((width, height, 2)).astype(np.float32)
+    _pos[:, :, 0] = _x
+    _pos[:, :, 1] = _y
+    return _pos
 
 
 def uint32(s) -> int:
-  _s = s if s > 0 else 0
-  return int(_s) if _s <= UINT_MAX else ctypes.c_uint32(int(_s)).value
+    _s = s if s > 0 else 0
+    return int(_s) if _s <= UINT_MAX else ctypes.c_uint32(int(_s)).value
 
 
 fu_pack = struct.Struct('>f')
@@ -44,38 +45,50 @@ fu_unpack = struct.Struct('>I')
 
 
 def floatBitsToUint(f: float) -> int:
-  fp = fu_pack.pack(f)
-  fu = fu_unpack.unpack(fp)[0]
-  return uint32(fu)
+    fp = fu_pack.pack(f)
+    fu = fu_unpack.unpack(fp)[0]
+    return uint32(fu)
 
 
 np_floatBitsToUint = np.vectorize(
-  floatBitsToUint, otypes=[np.uint32], cache=True)
+    floatBitsToUint, otypes=[np.uint32], cache=True)
 
 
 def uhash11(n: np.array) -> np.array:
-  n ^= (n << 1)
-  n ^= (n >> 1)
-  n *= k
-  n ^= (n << 1)
-  return n * k
+    n ^= (n << 1)
+    n ^= (n >> 1)
+    n *= k
+    n ^= (n << 1)
+    return n * k
 
 
 def hash11(p: np.array) -> np.array:
-  n = np_floatBitsToUint(p)
-  return uhash11(n).astype(np.float32) / float(UINT_MAX)
+    n = np_floatBitsToUint(p)
+    return uhash11(n).astype(np.float32) / float(UINT_MAX)
 
 
-#vec1 = np.empty((width_size, height_size, 1)).astype(np.float32)
-#vec2 = np.empty((width_size, height_size, 2)).astype(np.float32)
-#vec3 = np.empty((width_size, height_size, 3)).astype(np.float32)
+# vec1 = np.empty((width_size, height_size, 1)).astype(np.float32)
+# vec2 = np.empty((width_size, height_size, 2)).astype(np.float32)
+# vec3 = np.empty((width_size, height_size, 3)).astype(np.float32)
 
 pos = FragCoord(width_size, height_size)
-pprint(pos[0])
+# pprint(pos)
+# print('---')
+# pprint(pos[:, :, 0, ])
+# pprint(pos[:, :, 1, ])
+# print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+# pprint(pos[[1, 0], :])
+# pprint(pos[..., 1,])
+
+_pos = pos.copy()
+_pos[..., 0] = pos[..., 1]
+_pos[..., 1] = pos[..., 0]
+
+posnew = pos[..., np.newaxis]
 
 hash_x = hash11(pos[:, :, 0])
 
-#canvas_px = np.zeros((width_size, height_size, 3), dtype=np.uint8)
+# canvas_px = np.zeros((width_size, height_size, 3), dtype=np.uint8)
 canvas_px = np.zeros((width_size, height_size, 3)).astype(np.uint8)
 canvas_px[:, :, 0] = hash_x * RGB_SIZE
 canvas_px[:, :, 1] = hash_x * RGB_SIZE
@@ -83,5 +96,30 @@ canvas_px[:, :, 2] = hash_x * RGB_SIZE
 
 imgp = ImageP.fromarray(canvas_px)
 
-_ = 1
+# a = np.arange(18).reshape((3, 3, 2))
+# aa = a[:, 1:]
 
+a = np.arange(4)
+
+pprint(a)
+
+_a = a.copy()
+_a[1] = 100
+
+'''
+array(
+[
+    [
+        [0., 0.], [1., 0.], [2., 0.]
+    ],
+    [
+        [0., 1.], [1., 1.], [2., 1.]
+    ],
+    [
+        [0., 2.], [1., 2.], [2., 2.]
+    ]
+],
+)
+'''
+
+_ = 1
--- sandbox/memo.md
+++ sandbox/memo.md
@@ -1,7 +1,10 @@
+# 📝 2023/02/24
+
+[NumPyのファンシーインデックス（リストによる選択と代入） | note.nkmk.me](https://note.nkmk.me/python-numpy-fancy-indexing/)
+
 # 📝 2023/02/23
 
 [世界一分かりやすいnp.meshgridの使い方 (メッシュグリッド) | 機械学習と情報技術](https://disassemble-channel.com/np-meshgrid/)
-
 
 [NumPy入門 通常の関数をユニバーサル関数に変換する frompyfunc | Python学習講座](https://www.python.ambitious-engineer.com/archives/1333)
 
