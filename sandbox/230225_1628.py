f8a258dd1220dbc34a8d8f47197ce778811d3a49
pome-ta <pometarou93@gmail.com>
2023-02-25 17:19:02 +0900

    split window

--- sandbox/230225_1628.py
+++ sandbox/230225_1628.py
@@ -13,7 +13,7 @@ UINT_MAX = 0xffff_ffff
 k = np.array([0x456789ab, 0x6789ab45, 0x89ab4567]).astype(np.uint32)
 u = np.array([1, 2, 3]).astype(np.uint32)
 
-sq_size: int = 1024
+sq_size: int = 512
 
 width_size = sq_size
 height_size = sq_size
@@ -21,28 +21,28 @@ height_size = sq_size
 RGB_SIZE = 255
 color_ch = 3
 
-u_time = 0.123
+u_time = 0.4321
 
 
 @lru_cache()
 def _vec(w, h, c):
-  return np.empty((w, h, c)).astype(np.float32)
+    return np.empty((w, h, c)).astype(np.float32)
 
 
 def FragCoord(width, height) -> np.array:
-  _row = np.arange(0, width)
-  _col = np.arange(0, height).reshape(height, 1)
-  _x, _y = np.meshgrid(_row, _col)
-  _pos = _vec(width, height, 2)
-  _pos[:, :, 0] = _x
-  _pos[:, :, 1] = _y
-  return _pos
+    _row = np.arange(0, width)
+    _col = np.arange(0, height).reshape(height, 1)
+    _x, _y = np.meshgrid(_row, _col)
+    _pos = _vec(width, height, 2)
+    _pos[:, :, 0] = _x
+    _pos[:, :, 1] = _y
+    return _pos
 
 
 @lru_cache()
 def uint32(s) -> int:
-  _s = int(s if s > 0 else 0)
-  return _s if _s <= UINT_MAX else ctypes.c_uint32(_s).value
+    _s = int(s if s > 0 else 0)
+    return _s if _s <= UINT_MAX else ctypes.c_uint32(_s).value
 
 
 fu_pack = struct.Struct('>f')
@@ -51,126 +51,175 @@ fu_unpack = struct.Struct('>I')
 
 @lru_cache()
 def floatBitsToUint(f: float) -> int:
-  fp = fu_pack.pack(f)
-  fu = fu_unpack.unpack(fp)[0]
-  return fu  #uint32(fu)
+    fp = fu_pack.pack(f)
+    fu = fu_unpack.unpack(fp)[0]
+    return fu  # uint32(fu)
 
 
 np_floatBitsToUint = np.vectorize(
-  floatBitsToUint, otypes=[np.uint32], cache=True)
+    floatBitsToUint, otypes=[np.uint32], cache=True)
 
 
 def uhash11(n: np.array) -> np.array:
-  n ^= (n << u[0])
-  n ^= (n >> [0])
-  n *= k[0]
-  n ^= (n << [0])
-  return n * k[0]
+    n ^= (n << u[0])
+    n ^= (n >> [0])
+    n *= k[0]
+    n ^= (n << [0])
+    return n * k[0]
 
 
 def hash11(p: np.array) -> np.array:
-  n = np_floatBitsToUint(p)
-  return uhash11(n).astype(np.float32) / float(UINT_MAX)
+    n = np_floatBitsToUint(p)
+    return uhash11(n).astype(np.float32) / float(UINT_MAX)
 
 
 def uhash22(n: np.array) -> np.array:
-  _n = n.copy()
-  _n[..., 0] = n[..., 1]
-  _n[..., 1] = n[..., 0]
-  n ^= (_n << u[:2])
-
-  _n = n.copy()
-  _n[..., 0] = n[..., 1]
-  _n[..., 1] = n[..., 0]
-  n ^= (_n >> u[:2])
-
-  n *= k[:2]
-
-  _n = n.copy()
-  _n[..., 0] = n[..., 1]
-  _n[..., 1] = n[..., 0]
-  n ^= (_n << u[:2])
-  return n * k[:2]
+    _n = n.copy()
+    _n[..., 0] = n[..., 1]
+    _n[..., 1] = n[..., 0]
+    n ^= (_n << u[:2])
+    
+    _n = n.copy()
+    _n[..., 0] = n[..., 1]
+    _n[..., 1] = n[..., 0]
+    n ^= (_n >> u[:2])
+    
+    n *= k[:2]
+    
+    _n = n.copy()
+    _n[..., 0] = n[..., 1]
+    _n[..., 1] = n[..., 0]
+    n ^= (_n << u[:2])
+    return n * k[:2]
 
 
 def uhash33(n: np.array) -> np.array:
-  _n = n.copy()
-  _n[..., 0] = n[..., 1]
-  _n[..., 1] = n[..., 2]
-  _n[..., 2] = n[..., 0]
-  n ^= (_n << u)
-
-  _n = n.copy()
-  _n[..., 0] = n[..., 1]
-  _n[..., 1] = n[..., 2]
-  _n[..., 2] = n[..., 0]
-  n ^= (_n >> u)
-
-  n *= k
-
-  __n = n.copy()
-  _n[..., 0] = n[..., 1]
-  _n[..., 1] = n[..., 2]
-  _n[..., 2] = n[..., 0]
-  n ^= (_n << u)
-  return n * k
+    _n = n.copy()
+    _n[..., 0] = n[..., 1]
+    _n[..., 1] = n[..., 2]
+    _n[..., 2] = n[..., 0]
+    n ^= (_n << u)
+    
+    _n = n.copy()
+    _n[..., 0] = n[..., 1]
+    _n[..., 1] = n[..., 2]
+    _n[..., 2] = n[..., 0]
+    n ^= (_n >> u)
+    
+    n *= k
+    
+    __n = n.copy()
+    _n[..., 0] = n[..., 1]
+    _n[..., 1] = n[..., 2]
+    _n[..., 2] = n[..., 0]
+    n ^= (_n << u)
+    return n * k
 
 
 def hash22(p: np.array) -> np.array:
-  n = np_floatBitsToUint(p)
-  return uhash22(n).astype(np.float32) / float(UINT_MAX)
+    n = np_floatBitsToUint(p)
+    return uhash22(n).astype(np.float32) / float(UINT_MAX)
 
 
 def hash33(p: np.array) -> np.array:
-  n = np_floatBitsToUint(p)
-  return uhash33(n).astype(np.float32) / float(UINT_MAX)
+    n = np_floatBitsToUint(p)
+    return uhash33(n).astype(np.float32) / float(UINT_MAX)
 
 
 def hash21(p: np.array) -> np.array:
-  n = np_floatBitsToUint(p)
-  _h22 = uhash22(n).astype(np.float32)
-  return _h22[..., 0] / float(UINT_MAX)
+    n = np_floatBitsToUint(p)
+    _h22 = uhash22(n).astype(np.float32)
+    return _h22[..., 0] / float(UINT_MAX)
 
 
 def hash31(p: np.array) -> np.array:
-  n = np_floatBitsToUint(p)
-  _h33 = uhash33(n).astype(np.float32)
-  return _h33[..., 0] / float(UINT_MAX)
+    n = np_floatBitsToUint(p)
+    _h33 = uhash33(n).astype(np.float32)
+    return _h33[..., 0] / float(UINT_MAX)
 
 
-def profile_run():
+# def profile_run():
+#     pos = FragCoord(width_size, height_size)
+#     vec3 = _vec(width_size, height_size, 3)
+#     vec3[..., 0] = pos[..., 0]
+#     vec3[..., 1] = pos[..., 1]
+#     vec3[..., 2] = u_time
+#
+#     h21 = hash21(pos)
+#     h22 = hash22(pos)
+#     h31 = hash31(vec3)
+#     h33 = hash33(vec3)
+#
+#     canvas_px = np.zeros((width_size, height_size, 3)).astype(np.uint8)
+#     '''
+#     canvas_px[:, :, 0] = hash_xy[:, :, 0] * RGB_SIZE
+#     canvas_px[:, :, 1] = hash_xy[..., 1] * RGB_SIZE
+#     canvas_px[:, :, 2] = RGB_SIZE
+#     '''
+#     '''
+#     canvas_px[:, :, 0] = h21 * RGB_SIZE
+#     canvas_px[:, :, 1] = h21 * RGB_SIZE
+#     canvas_px[:, :, 2] = h21 * RGB_SIZE
+#     '''
+#
+#     canvas_px[:, :, 0] = h33[..., 0] * RGB_SIZE
+#     canvas_px[:, :, 1] = h33[..., 1] * RGB_SIZE
+#     canvas_px[:, :, 2] = h33[..., 2] * RGB_SIZE
+#
+#     imgp = ImageP.fromarray(canvas_px)
+#
+#
+# cProfile.run('profile_run()', sort=1)
 
-  pos = FragCoord(width_size, height_size)
-  vec3 = _vec(width_size, height_size, 3)
-  vec3[..., 0] = pos[..., 0]
-  vec3[..., 1] = pos[..., 1]
-  vec3[..., 2] = u_time
+pos = FragCoord(width_size, height_size)
+vec3 = _vec(width_size, height_size, 3)
+vec3[..., 0] = pos[..., 0]
+vec3[..., 1] = pos[..., 1]
+vec3[..., 2] = u_time
 
-  h21 = hash21(pos)
-  h22 = hash22(pos)
-  h31 = hash31(vec3)
-  h33 = hash33(vec3)
+h21 = hash21(pos)
+h22 = hash22(pos)
+h31 = hash31(vec3)
+h33 = hash33(vec3)
 
-  canvas_px = np.zeros((width_size, height_size, 3)).astype(np.uint8)
-  '''
-  canvas_px[:, :, 0] = hash_xy[:, :, 0] * RGB_SIZE
-  canvas_px[:, :, 1] = hash_xy[..., 1] * RGB_SIZE
-  canvas_px[:, :, 2] = RGB_SIZE
-  '''
-  '''
-  canvas_px[:, :, 0] = h21 * RGB_SIZE
-  canvas_px[:, :, 1] = h21 * RGB_SIZE
-  canvas_px[:, :, 2] = h21 * RGB_SIZE
-  '''
+canvas_px = np.zeros((width_size, height_size, 3)).astype(np.uint8)
+'''
+canvas_px[:, :, 0] = hash_xy[:, :, 0] * RGB_SIZE
+canvas_px[:, :, 1] = hash_xy[..., 1] * RGB_SIZE
+canvas_px[:, :, 2] = RGB_SIZE
+'''
+'''
+canvas_px[:, :, 0] = h21 * RGB_SIZE
+canvas_px[:, :, 1] = h21 * RGB_SIZE
+canvas_px[:, :, 2] = h21 * RGB_SIZE
+'''
 
-  canvas_px[:, :, 0] = h33[..., 0] * RGB_SIZE
-  canvas_px[:, :, 1] = h33[..., 1] * RGB_SIZE
-  canvas_px[:, :, 2] = h33[..., 2] * RGB_SIZE
+split_div = int(sq_size / 2)
+for div in range(4):
+    if div == 0:
+        canvas_px[:split_div, :split_div, 0] = h21[:split_div, :split_div] * RGB_SIZE
+        canvas_px[:split_div, :split_div, 1] = h21[:split_div, :split_div] * RGB_SIZE
+        canvas_px[:split_div, :split_div, 2] = h21[:split_div, :split_div] * RGB_SIZE
+    elif div == 1:
+        canvas_px[split_div:, :split_div, 0] = h22[split_div:, :split_div, 0] * RGB_SIZE
+        canvas_px[split_div:, :split_div, 1] = h22[split_div:, :split_div, 1] * RGB_SIZE
+        canvas_px[split_div:, :split_div, 2] = RGB_SIZE
+    elif div == 2:
+        canvas_px[:split_div, split_div:, 0] = h31[:split_div, split_div:] * RGB_SIZE
+        canvas_px[:split_div, split_div:, 1] = h31[:split_div, split_div:] * RGB_SIZE
+        canvas_px[:split_div, split_div:, 2] = h31[:split_div, split_div:] * RGB_SIZE
+    elif div == 3:
+        canvas_px[split_div:, split_div:, 0] = h33[split_div:, split_div:, 0] * RGB_SIZE
+        canvas_px[split_div:, split_div:, 1] = h33[split_div:, split_div:, 1] * RGB_SIZE
+        canvas_px[split_div:, split_div:, 2] = h33[split_div:, split_div:, 2] * RGB_SIZE
 
-  imgp = ImageP.fromarray(canvas_px)
+# canvas_px[:split_div, :split_div, 0] = h33[:split_div, :split_div, 0] * RGB_SIZE
+# canvas_px[int(sq_size / 2):, int(sq_size / 2):, 0] = h33[int(sq_size / 2):, int(sq_size / 2):, 0] * RGB_SIZE
+# canvas_px[:, :, 1] = h33[:, :, 1] * RGB_SIZE
+# canvas_px[:, :, 2] = h33[:, :, 2] * RGB_SIZE
 
 
-cProfile.run('profile_run()', sort=1)
+imgp = ImageP.fromarray(canvas_px)
+imgp.show()
 
 _ = 1
-
--- sandbox/memo.md
+++ sandbox/memo.md
@@ -1,3 +1,82 @@
+# 📝 2023/02/25
+
+```text
+         12580047 function calls (12580046 primitive calls) in 8.241 seconds
+
+   Ordered by: internal time
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        4    2.723    0.681    7.672    1.918 function_base.py:2400(_vectorize_call)
+  4193297    2.685    0.000    4.209    0.000 230225_1628.py:52(floatBitsToUint)
+  4193297    0.809    0.000    0.809    0.000 {method 'unpack' of '_struct.Struct' objects}
+       10    0.741    0.074    0.741    0.074 {built-in method numpy.asanyarray}
+  4193297    0.715    0.000    0.715    0.000 {method 'pack' of '_struct.Struct' objects}
+        4    0.171    0.043    7.843    1.961 function_base.py:2301(__call__)
+        2    0.151    0.075    0.170    0.085 230225_1628.py:96(uhash33)
+        2    0.114    0.057    0.120    0.060 230225_1628.py:76(uhash22)
+        7    0.052    0.007    0.052    0.007 {method 'astype' of 'numpy.ndarray' objects}
+       14    0.035    0.002    0.035    0.002 {method 'copy' of 'numpy.ndarray' objects}
+        1    0.020    0.020    8.239    8.239 230225_1628.py:141(profile_run)
+        1    0.005    0.005    2.102    2.102 230225_1628.py:135(hash31)
+        1    0.004    0.004    0.004    0.004 {built-in method numpy.zeros}
+        1    0.004    0.004    2.009    2.009 230225_1628.py:124(hash33)
+        2    0.003    0.002    0.039    0.020 230225_1628.py:27(_vec)
+        1    0.003    0.003    2.243    2.243 230225_1628.py:129(hash21)
+        1    0.003    0.003    0.031    0.031 230225_1628.py:32(FragCoord)
+        1    0.002    0.002    8.241    8.241 <string>:1(<module>)
+        1    0.002    0.002    1.803    1.803 230225_1628.py:119(hash22)
+        1    0.001    0.001    0.001    0.001 {method 'decode' of 'ImagingDecoder' objects}
+        1    0.000    0.000    0.000    0.000 {built-in method PIL._imaging.fill}
+        1    0.000    0.000    8.241    8.241 {built-in method builtins.exec}
+        2    0.000    0.000    0.000    0.000 stride_tricks.py:340(_broadcast_to)
+        1    0.000    0.000    0.001    0.001 Image.py:3030(fromarray)
+        2    0.000    0.000    0.000    0.000 {built-in method numpy.empty}
+        4    0.000    0.000    0.000    0.000 function_base.py:2331(_get_ufunc_and_otypes)
+        4    0.000    0.000    0.377    0.094 function_base.py:2410(<listcomp>)
+        1    0.000    0.000    0.000    0.000 {built-in method PIL._imaging.raw_decoder}
+        1    0.000    0.000    0.000    0.000 Image.py:2896(new)
+        1    0.000    0.000    0.000    0.000 {built-in method numpy.frompyfunc}
+        1    0.000    0.000    0.010    0.010 function_base.py:4892(meshgrid)
+        2    0.000    0.000    0.000    0.000 {built-in method numpy.arange}
+        1    0.000    0.000    0.000    0.000 stride_tricks.py:416(_broadcast_shape)
+        1    0.000    0.000    0.000    0.000 stride_tricks.py:480(broadcast_arrays)
+        1    0.000    0.000    0.000    0.000 Image.py:543(_new)
+        1    0.000    0.000    0.000    0.000 Image.py:393(_getdecoder)
+        1    0.000    0.000    0.001    0.001 Image.py:2935(frombytes)
+        1    0.000    0.000    0.001    0.001 Image.py:807(frombytes)
+        2    0.000    0.000    0.000    0.000 Image.py:512(__init__)
+        1    0.000    0.000    0.010    0.010 function_base.py:5045(<listcomp>)
+       17    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+        1    0.000    0.000    0.010    0.010 <__array_function__ internals>:177(meshgrid)
+        3    0.000    0.000    0.000    0.000 Image.py:2875(_check_size)
+        1    0.000    0.000    0.000    0.000 function_base.py:5032(<listcomp>)
+      2/1    0.000    0.000    0.010    0.010 {built-in method numpy.core._multiarray_umath.implement_array_function}
+        3    0.000    0.000    0.000    0.000 {method 'reshape' of 'numpy.ndarray' objects}
+        1    0.000    0.000    0.001    0.001 Image.py:2973(frombuffer)
+        7    0.000    0.000    0.000    0.000 {built-in method builtins.isinstance}
+        1    0.000    0.000    0.000    0.000 {method 'setimage' of 'ImagingDecoder' objects}
+        1    0.000    0.000    0.000    0.000 stride_tricks.py:546(<listcomp>)
+        4    0.000    0.000    0.000    0.000 {method 'setdefault' of 'dict' objects}
+        1    0.000    0.000    0.000    0.000 stride_tricks.py:538(<listcomp>)
+        2    0.000    0.000    0.000    0.000 function_base.py:346(iterable)
+        1    0.000    0.000    0.000    0.000 <__array_function__ internals>:177(broadcast_arrays)
+        2    0.000    0.000    0.000    0.000 {built-in method builtins.any}
+        2    0.000    0.000    0.000    0.000 stride_tricks.py:542(<genexpr>)
+        4    0.000    0.000    0.000    0.000 {built-in method numpy.array}
+        6    0.000    0.000    0.000    0.000 stride_tricks.py:345(<genexpr>)
+        2    0.000    0.000    0.000    0.000 stride_tricks.py:25(_maybe_view_as_subclass)
+        2    0.000    0.000    0.000    0.000 {method '__exit__' of 'numpy.nditer' objects}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.getattr}
+        1    0.000    0.000    0.000    0.000 {built-in method builtins.all}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+        2    0.000    0.000    0.000    0.000 {built-in method builtins.iter}
+        1    0.000    0.000    0.000    0.000 {method 'copy' of 'dict' objects}
+        1    0.000    0.000    0.000    0.000 function_base.py:4887(_meshgrid_dispatcher)
+        1    0.000    0.000    0.000    0.000 {method 'get' of 'dict' objects}
+        1    0.000    0.000    0.000    0.000 stride_tricks.py:476(_broadcast_arrays_dispatcher)
+
+```
+
 # 📝 2023/02/24
 
 [NumPyのファンシーインデックス（リストによる選択と代入） | note.nkmk.me](https://note.nkmk.me/python-numpy-fancy-indexing/)
