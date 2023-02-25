add39292ccf1e497827493aa20a38cb6427ba99e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-02-25 13:31:20 +0900

    up

--- sandbox/230225_0058.py
+++ sandbox/230225_0058.py
@@ -5,23 +5,17 @@ from PIL import Image as ImageP
 from pprint import pprint
 
 UINT_MAX = 0xffff_ffff
-k = 0x4567_89ab
 
-sq_size: int = 3
+k = np.array([0x456789ab, 0x6789ab45, 0x89ab4567]).astype(np.uint32)
+u = np.array([1, 2, 3]).astype(np.uint32)
+
+sq_size: int = 1024
 
 width_size = sq_size
 height_size = sq_size
 
 RGB_SIZE = 255
 color_ch = 3
-
-#
-# def _vec(x):
-#     _vec_array = np.empty(x).astype(np.float32)
-#
-#
-# def vec(x):
-#     pass
 
 
 def FragCoord(width, height) -> np.array:
@@ -51,8 +45,7 @@ def floatBitsToUint(f: float) -> int:
 
 np_floatBitsToUint = np.vectorize(
   floatBitsToUint, otypes=[np.uint32], cache=True)
-
-
+'''
 def uhash11(n: np.array) -> np.array:
   n ^= (n << 1)
   n ^= (n >> 1)
@@ -64,61 +57,48 @@ def uhash11(n: np.array) -> np.array:
 def hash11(p: np.array) -> np.array:
   n = np_floatBitsToUint(p)
   return uhash11(n).astype(np.float32) / float(UINT_MAX)
+'''
 
 
-# vec1 = np.empty((width_size, height_size, 1)).astype(np.float32)
-# vec2 = np.empty((width_size, height_size, 2)).astype(np.float32)
-# vec3 = np.empty((width_size, height_size, 3)).astype(np.float32)
+def uhash22(n: np.array) -> np.array:
+  _n = n.copy()
+  _n[..., 0] = n[..., 1]
+  _n[..., 1] = n[..., 0]
+  n ^= (_n << u[:2])
+
+  _n = n.copy()
+  _n[..., 0] = n[..., 1]
+  _n[..., 1] = n[..., 0]
+  n ^= (_n >> u[:2])
+
+  n *= k[:2]
+
+  _n = n.copy()
+  _n[..., 0] = n[..., 1]
+  _n[..., 1] = n[..., 0]
+  n ^= (_n << u[:2])
+  return n * k[:2]
+
+
+def hash22(p: np.array) -> np.array:
+  n = np_floatBitsToUint(p)
+  return uhash22(n).astype(np.float32) / float(UINT_MAX)
+
 
 pos = FragCoord(width_size, height_size)
-# pprint(pos)
-# print('---')
-# pprint(pos[:, :, 0, ])
-# pprint(pos[:, :, 1, ])
-# print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-# pprint(pos[[1, 0], :])
-# pprint(pos[..., 1,])
 
-_pos = pos.copy()
-_pos[..., 0] = pos[..., 1]
-_pos[..., 1] = pos[..., 0]
-
-posnew = pos[..., np.newaxis]
-
-hash_x = hash11(pos[:, :, 0])
+hash_xy = hash22(pos)
 
 # canvas_px = np.zeros((width_size, height_size, 3), dtype=np.uint8)
 canvas_px = np.zeros((width_size, height_size, 3)).astype(np.uint8)
-canvas_px[:, :, 0] = hash_x * RGB_SIZE
-canvas_px[:, :, 1] = hash_x * RGB_SIZE
-canvas_px[:, :, 2] = hash_x * RGB_SIZE
+canvas_px[:, :, 0] = hash_xy[..., 0] * RGB_SIZE
+canvas_px[:, :, 1] = hash_xy[..., 1] * RGB_SIZE
+canvas_px[:, :, 2] = RGB_SIZE
 
 imgp = ImageP.fromarray(canvas_px)
 
 # a = np.arange(18).reshape((3, 3, 2))
 # aa = a[:, 1:]
-
-a = np.arange(4)
-
-pprint(a)
-
-_a = a.copy()
-_a[1] = 100
-'''
-array(
-[
-    [
-        [0., 0.], [1., 0.], [2., 0.]
-    ],
-    [
-        [0., 1.], [1., 1.], [2., 1.]
-    ],
-    [
-        [0., 2.], [1., 2.], [2., 2.]
-    ]
-],
-)
-'''
 
 _ = 1
 
