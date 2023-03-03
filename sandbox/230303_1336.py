2368dc9d4d56b2aebb62518b136ccdde3e792c9b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-03 17:03:57 +0900

    up

--- sandbox/230303_1336.py
+++ sandbox/230303_1336.py
@@ -1,4 +1,15 @@
 import numpy as np
+from PIL import Image as ImageP
 
-sq_size = 256
+sq_size = 512
+RGB_SIZE = 255
+COLOR_CH = 3
 
+def create_img(*args):
+  print(args)
+
+canvas_px = np.zeros((sq_size, sq_size, COLOR_CH)).astype(np.uint8)
+
+create_img(0)
+
+imgp = ImageP.fromarray(canvas_px)
