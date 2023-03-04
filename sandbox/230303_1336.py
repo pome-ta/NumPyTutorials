ed7c09bfed319a31c6b7a7d4773592c5721947df
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-04 09:16:43 +0900

    up

--- sandbox/230303_1336.py
+++ sandbox/230303_1336.py
@@ -5,11 +5,14 @@ sq_size = 512
 RGB_SIZE = 255
 COLOR_CH = 3
 
+
 def create_img(*args):
   print(args)
+
 
 canvas_px = np.zeros((sq_size, sq_size, COLOR_CH)).astype(np.uint8)
 
 create_img(0)
 
 imgp = ImageP.fromarray(canvas_px)
+
