f1868c6fcfbda59874e84dc9460b66c70dfe8a88
pome-ta <pometarou93@gmail.com>
2023-03-06 15:27:30 +0900

    file move

---
+++ sandbox/glslLike/00_basicTest/230222_1041.py
@@ -0,0 +1,31 @@
+import numpy as np
+from PIL import Image as ImageP
+
+UINT_MAX = 0xffffffff
+
+sq_size = 1280
+RGB_SIZE = 256
+color_ch = 3
+
+length_size = sq_size * sq_size * color_ch
+
+# xyz = np.arange(0, length_size, dtype=np.uint8).reshape(sq_size, sq_size, color_ch)
+# xyz = np.arange(0, 10)
+# zoro_uint8 = np.zeros((sq_size, sq_size, 3), dtype=np.uint8)
+
+# xyz = np.arange(0, length_size,RGB_SIZE)  #.reshape(sq_size, sq_size, color_ch)
+
+# imgp = ImageP.fromarray(xyz)
+
+# print(sq_size/RGB_SIZE * length_size)
+
+ll = np.linspace(0, RGB_SIZE, num=length_size, endpoint=False)
+
+ll_size = ll.size
+
+uary = np.array(ll, dtype=np.uint8).reshape(sq_size, sq_size, color_ch)
+u_size = uary.size
+
+imgp = ImageP.fromarray(uary)
+
+_ = 1
--- sandbox/glslLike/00_basicTest/230222_1155.py
+++ sandbox/glslLike/00_basicTest/230222_1155.py
@@ -9,21 +9,21 @@ color_ch = 3
 
 length_size = sq_size * sq_size * color_ch
 
-#xyz = np.arange(0, length_size, dtype=np.uint8).reshape(sq_size, sq_size, color_ch)
-#xyz = np.arange(0, 10)
-#zoro_uint8 = np.zeros((sq_size, sq_size, 3), dtype=np.uint8)
+# xyz = np.arange(0, length_size, dtype=np.uint8).reshape(sq_size, sq_size, color_ch)
+# xyz = np.arange(0, 10)
+# zoro_uint8 = np.zeros((sq_size, sq_size, 3), dtype=np.uint8)
 
-#xyz = np.arange(0, length_size,RGB_SIZE)  #.reshape(sq_size, sq_size, color_ch)
+# xyz = np.arange(0, length_size,RGB_SIZE)  #.reshape(sq_size, sq_size, color_ch)
 
-#imgp = ImageP.fromarray(xyz)
+# imgp = ImageP.fromarray(xyz)
 
-#print(sq_size/RGB_SIZE * length_size)
+# print(sq_size/RGB_SIZE * length_size)
 
 ll = np.linspace(0, RGB_SIZE, num=length_size, endpoint=False)
 
 ll_size = ll.size
 
-uary =np.array(ll, dtype=np.uint8).reshape(sq_size, sq_size, color_ch)
+uary = np.array(ll, dtype=np.uint8).reshape(sq_size, sq_size, color_ch)
 u_size = uary.size
 
 cr_np = np.full((sq_size, sq_size), 1, dtype=np.uint8)
@@ -40,4 +40,3 @@ _ = 1
 1111_1111
 1111_1111
 '''
-
