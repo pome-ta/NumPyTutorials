8c0cef5b08db6b8246c074e4b0712ab2db422611
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-10-27 13:49:32 +0900

    UInt16

--- simd4metal/231027_0103.py
+++ simd4metal/231027_0103.py
@@ -4,32 +4,21 @@ from dataclasses import dataclass
 import numpy as np
 
 Float = np.dtype(np.float32)
+UInt16 = np.dtype(np.uint16)
 
 oldVertices = np.array(
-  (
-    # yapf: disable
-    -1,
-    1,
-    0,
-    1,
-    -1,
-    0,
-    -1,
-    -1,
-    0,
-    -1,
-    1,
-    0,
-    1,
-    1,
-    0,
-    1,
-    -1,
-    0),
-  dtype=Float)
-# yapf: enable
+  (-1,  1,  0,  # triangle 1
+    1, -1,  0,
+   -1, -1,  0,
+   -1,  1,  0,  # triangle 2
+    1,  1,  0,
+    1, -1,  0,),
+  dtype=Float)  # yapf: disable
 
+scale = 0.8
+vertices = oldVertices * scale
 #print(dir(oldVertices))
 print(oldVertices.strides)
 print(oldVertices.size)
+print(UInt16.itemsize)
 
