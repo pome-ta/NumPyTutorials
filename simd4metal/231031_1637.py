3b81076000798d29a3d527aa6fd91a80c90f36e9
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-11-04 13:47:45 +0900

    up

--- simd4metal/231031_1637.py
+++ simd4metal/231031_1637.py
@@ -22,12 +22,34 @@ simd_float3 = np.dtype({
 }, align=True)  # yapf: disable
 
 
-_vertices = [
-  [-1,  1,  0,],
-  [ 1,  1,  0,],
-  [ 1, -1,  0,],
-  [ 1, -1,  0,],
-]  # # yapf: disable
+scale = 0.8
+
+vertices_np = np.array(
+  [
+    np.array((-1,  1,  0,), dtype=PackedFloat3),
+    np.array(( 1,  1,  0,), dtype=PackedFloat3),
+    np.array((-1, -1,  0,), dtype=PackedFloat3),
+    np.array(( 1, -1,  0,), dtype=PackedFloat3),
+  ]
+)  # yapf: disable
+
+#pl3 = np.array((-1,  1,  0,), dtype=PackedFloat3)
+#pl3['x'] *= scale
+
+#print(pl3['x'])
+#print(dir(pl3['x']))
+'''
+for pl3 in vertices_np:
+  #print(pl3)
+  print(dir(pl3))
+'''
+
+#[[pl3[n]] for pl3 in vertices_np]
+#print(PackedFloat3.names)
+
+for pl3 in vertices_np:
+  for name in PackedFloat3.names:
+    pl3[name] *= scale
 
 
-vertices_py = [[vc * cs]]
+print(vertices_np.size)
