a363738283e39322ac1c40f765cca2946c42cd3e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-07 01:01:31 +0900

    h11

--- sandbox/glslLike/02_hash/231207_0041.py
+++ sandbox/glslLike/02_hash/231207_0041.py
@@ -4,35 +4,18 @@ from functools import lru_cache
 import numpy as np
 from PIL import Image as ImageP
 
+# ハッシュ値関係
 UINT_MAX = 0xffff_ffff
+k = 0x4567_89ab
 
-k = np.array([0x456789ab, 0x6789ab45, 0x89ab4567]).astype(np.uint32)
-u = np.array([1, 2, 3]).astype(np.uint32)
-
+# キャンバスサイズ関係
 sq_size: int = 128
-
 width_size = sq_size
 height_size = sq_size
 
+# キャンバスカラー関係
 RGB_SIZE = 255
 color_ch = 3
-
-u_time = 0.4321
-
-
-@lru_cache()
-def _vec(w, h, c):
-  return np.empty((w, h, c)).astype(np.float32)
-
-
-def FragCoord(width, height) -> np.array:
-  _row = np.arange(0, width)
-  _col = np.arange(0, height).reshape(height, 1)
-  _x, _y = np.meshgrid(_row, _col)
-  _pos = _vec(width, height, 2)
-  _pos[:, :, 0] = _x
-  _pos[:, :, 1] = _y
-  return _pos
 
 
 fu_pack = struct.Struct('>f')
@@ -50,11 +33,11 @@ np_floatBitsToUint = np.vectorize(floatBitsToUint,
 
 
 def uhash11(n: np.array) -> np.array:
-  n ^= (n << u[0])
-  n ^= (n >> u[0])
-  n *= k[0]
-  n ^= (n << u[0])
-  return n * k[0]
+  n ^= (n << 1)
+  n ^= (n >> 1)
+  n *= k
+  n ^= (n << 1)
+  return n * k
 
 
 def hash11(p: np.array) -> np.array:
@@ -62,41 +45,7 @@ def hash11(p: np.array) -> np.array:
   return uhash11(n).astype(np.float32) / float(UINT_MAX)
 
 
-def uhash22(n: np.array) -> np.array:
-  _n = n.copy()
-  _n[..., 0] = n[..., 1]
-  _n[..., 1] = n[..., 0]
-  n ^= (_n << u[:2])
-
-  _n = n.copy()
-  _n[..., 0] = n[..., 1]
-  _n[..., 1] = n[..., 0]
-  n ^= (_n >> u[:2])
-
-  n *= k[:2]
-
-  _n = n.copy()
-  _n[..., 0] = n[..., 1]
-  _n[..., 1] = n[..., 0]
-  n ^= (_n << u[:2])
-  return n * k[:2]
-
-
-def hash22(p: np.array) -> np.array:
-  n = np_floatBitsToUint(p)
-  return uhash22(n).astype(np.float32) / float(UINT_MAX)
-
-
-def hash21(p: np.array) -> np.array:
-  n = np_floatBitsToUint(p)
-  _h22 = uhash22(n).astype(np.float32)
-  return _h22[..., 0] / float(UINT_MAX)
-
-
-pos = FragCoord(width_size, height_size)
 h11 = hash11([list(range(sq_size))])
-h21 = hash21(pos)
-#h11 = hash11(pos[0])
 
 canvas_px = np.zeros((width_size, height_size, 3)).astype(np.uint8)
 
