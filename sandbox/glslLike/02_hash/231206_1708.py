9718d6fbe584e1afc30f29d64c71e1be54559926
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-06 17:09:08 +0900

    h21

---
+++ sandbox/glslLike/02_hash/231206_1708.py
@@ -0,0 +1,147 @@
+import struct
+from functools import lru_cache
+
+import numpy as np
+from PIL import Image as ImageP
+
+UINT_MAX = 0xffff_ffff
+
+k = np.array([0x456789ab, 0x6789ab45, 0x89ab4567]).astype(np.uint32)
+u = np.array([1, 2, 3]).astype(np.uint32)
+
+sq_size: int = 512
+
+width_size = sq_size
+height_size = sq_size
+
+RGB_SIZE = 255
+color_ch = 3
+
+u_time = 0.4321
+
+
+@lru_cache()
+def _vec(w, h, c):
+  return np.empty((w, h, c)).astype(np.float32)
+
+
+def FragCoord(width, height) -> np.array:
+  _row = np.arange(0, width)
+  _col = np.arange(0, height).reshape(height, 1)
+  _x, _y = np.meshgrid(_row, _col)
+  _pos = _vec(width, height, 2)
+  _pos[:, :, 0] = _x
+  _pos[:, :, 1] = _y
+  return _pos
+
+
+fu_pack = struct.Struct('>f')
+fu_unpack = struct.Struct('>I')
+
+
+@lru_cache()
+def floatBitsToUint(f: float) -> int:
+  return fu_unpack.unpack(fu_pack.pack(f))[0]
+
+
+np_floatBitsToUint = np.vectorize(floatBitsToUint,
+                                  otypes=[np.uint32],
+                                  cache=True)
+
+
+def uhash11(n: np.array) -> np.array:
+  n ^= (n << u[0])
+  n ^= (n >> [0])
+  n *= k[0]
+  n ^= (n << [0])
+  return n * k[0]
+
+
+def hash11(p: np.array) -> np.array:
+  n = np_floatBitsToUint(p)
+  return uhash11(n).astype(np.float32) / float(UINT_MAX)
+
+
+def uhash22(n: np.array) -> np.array:
+  _n = n.copy()
+  _n[..., 0] = n[..., 1]
+  _n[..., 1] = n[..., 0]
+  n ^= (_n << u[:2])
+
+  _n = n.copy()
+  _n[..., 0] = n[..., 1]
+  _n[..., 1] = n[..., 0]
+  n ^= (_n >> u[:2])
+
+  n *= k[:2]
+
+  _n = n.copy()
+  _n[..., 0] = n[..., 1]
+  _n[..., 1] = n[..., 0]
+  n ^= (_n << u[:2])
+  return n * k[:2]
+
+
+def uhash33(n: np.array) -> np.array:
+  _n = n.copy()
+  _n[..., 0] = n[..., 1]
+  _n[..., 1] = n[..., 2]
+  _n[..., 2] = n[..., 0]
+  n ^= (_n << u)
+
+  _n = n.copy()
+  _n[..., 0] = n[..., 1]
+  _n[..., 1] = n[..., 2]
+  _n[..., 2] = n[..., 0]
+  n ^= (_n >> u)
+
+  n *= k
+
+  __n = n.copy()
+  _n[..., 0] = n[..., 1]
+  _n[..., 1] = n[..., 2]
+  _n[..., 2] = n[..., 0]
+  n ^= (_n << u)
+  return n * k
+
+
+def hash22(p: np.array) -> np.array:
+  n = np_floatBitsToUint(p)
+  return uhash22(n).astype(np.float32) / float(UINT_MAX)
+
+
+def hash33(p: np.array) -> np.array:
+  n = np_floatBitsToUint(p)
+  return uhash33(n).astype(np.float32) / float(UINT_MAX)
+
+
+def hash21(p: np.array) -> np.array:
+  n = np_floatBitsToUint(p)
+  _h22 = uhash22(n).astype(np.float32)
+  return _h22[..., 0] / float(UINT_MAX)
+
+
+def hash31(p: np.array) -> np.array:
+  n = np_floatBitsToUint(p)
+  _h33 = uhash33(n).astype(np.float32)
+  return _h33[..., 0] / float(UINT_MAX)
+
+
+pos = FragCoord(width_size, height_size)
+vec3 = _vec(width_size, height_size, 3)
+vec3[..., 0] = pos[..., 0]
+vec3[..., 1] = pos[..., 1]
+vec3[..., 2] = u_time
+
+h21 = hash21(pos)
+
+canvas_px = np.zeros((width_size, height_size, 3)).astype(np.uint8)
+
+for c in range(color_ch):
+  canvas_px[:, :, c] = h21 * RGB_SIZE
+
+imgp = ImageP.fromarray(canvas_px)
+imgp.show()
+
+_ = 1
+
