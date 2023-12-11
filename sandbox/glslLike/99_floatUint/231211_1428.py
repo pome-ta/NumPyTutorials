c0e0145028584a2541cfb8c8e79cbf25593a17c0
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-11 16:43:07 +0900

    cast

--- sandbox/glslLike/99_floatUint/231211_1428.py
+++ sandbox/glslLike/99_floatUint/231211_1428.py
@@ -1,8 +1,8 @@
 import struct
 import numpy as np
 
-fu_pack = struct.Struct(">f")
-fu_unpack = struct.Struct(">I")
+#fu_pack = struct.Struct(">f")
+#fu_unpack = struct.Struct(">I")
 '''
 np_floatBitsToUint = np.vectorize(
   lambda f: fu_unpack.unpack(fu_pack.pack(f))[0],
@@ -13,9 +13,11 @@ np_floatBitsToUint = np.vectorize(
 
 def floatBitsToUint(f: float) -> int:
   #return fu_unpack.unpack(fu_pack.pack(f))[0]
-  f_p = fu_pack.pack(f)
+  #f_p = fu_pack.pack(f)
+  f_p = struct.pack('>f', f)
   print(f'{f_p=}')
-  f_u = fu_unpack.unpack(f_p)
+  #f_u = fu_unpack.unpack(f_p)
+  f_u = struct.unpack('>I', f_p)
   print(f'{f_u=}')
   return f_u[0]
 
@@ -42,9 +44,9 @@ def hash11(p: list[float]) -> np.ndarray:
 
 
 if __name__ == "__main__":
-  #size: int = 8
-  #l_float: list[float] = [float(f) for f in range(size)]
-  #h11 = hash11(l_float)
-  h11 = hash11([1.0])
+  size: int = 8
+  l_float: list[float] = [float(f) for f in range(size)]
+  h11 = hash11(l_float)
+  #h11 = hash11([1.0])
   print(f'{h11=}')
 
--- sandbox/glslLike/99_floatUint/memo.md
+++ sandbox/glslLike/99_floatUint/memo.md
@@ -2,3 +2,16 @@
 
 ## `floatBitsToUint` を思い出す
 
+[struct.pack | struct --- バイト列をパックされたバイナリデータとして解釈する — Python 3.10.13 ドキュメント](https://docs.python.org/ja/3.10/library/struct.html#struct.pack)
+
+[struct.unpack | struct --- バイト列をパックされたバイナリデータとして解釈する — Python 3.10.13 ドキュメント](https://docs.python.org/ja/3.10/library/struct.html#struct.unpack)
+
+
+### バイトオーダー
+
+[byte-order-size-and-alignment | struct --- バイト列をパックされたバイナリデータとして解釈する — Python 3.10.13 ドキュメント](https://docs.python.org/ja/3.10/library/struct.html#byte-order-size-and-alignment)
+
+
+### 指定文字列（C の型）
+
+[format-characters | struct --- バイト列をパックされたバイナリデータとして解釈する — Python 3.10.13 ドキュメント](https://docs.python.org/ja/3.10/library/struct.html#format-characters)
